c20163392b6962d14586a1dd6898ebe8ea4b52eb
Jonathan C. Calixto <jonathan.calixto@maxmilhas.com.br>
2020-04-02 15:25:37 -0300

    Defining create incidents route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const OngsController = require('./controllers/OngsController');
+const IncidentsController = require('./controllers/IncidentsController');
 
 const routes = express.Router();
 
 // Ongs routes
 routes.get('/ongs', OngsController.index);
 routes.post('/ongs', OngsController.create);
+
+// Incidents routes
+routes.post('/incidents', IncidentsController.create);
 
 module.exports = routes;
