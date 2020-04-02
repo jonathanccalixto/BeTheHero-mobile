1bc51e6580b809d9cd32490a5a74e86a866ffd8e
Jonathan C. Calixto <jonathan.calixto@maxmilhas.com.br>
2020-04-02 16:46:11 -0300

    Refactory routes. Reorganize routes in alphabetc order

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 
-const OngsController = require('./controllers/OngsController');
 const IncidentsController = require('./controllers/IncidentsController');
+const OngsController = require('./controllers/OngsController');
 
 const routes = express.Router();
-
-// Ongs routes
-routes.get('/ongs', OngsController.index);
-routes.post('/ongs', OngsController.create);
 
 // Incidents routes
 routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
 routes.delete('/incidents/:id', IncidentsController.delete);
+
+// Ongs routes
+routes.get('/ongs', OngsController.index);
+routes.post('/ongs', OngsController.create);
 
 module.exports = routes;
