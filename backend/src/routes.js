268d4ff40609a23635d667ad6f4edfa14beea250
Jonathan C. Calixto <jonathan.calixto@maxmilhas.com.br>
2020-04-02 09:20:43 -0300

    Split controllers at routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,31 +1,11 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngsController = require('./controllers/OngsController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async (request, response) => {
-  const ongs = await connection('ongs').select('*');
-
-  return response.json(ongs);
-})
-
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  return response.json({ id });
-});
+// Ongs routes
+routes.get('/ongs', OngsController.index);
+routes.post('/ongs', OngsController.create);
 
 module.exports = routes;
