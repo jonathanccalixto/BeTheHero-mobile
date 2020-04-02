7eac276cc5d3f0dd0663d883cf59b582c860d5ba
Jonathan C. Calixto <jonathan.calixto@maxmilhas.com.br>
2020-04-01 22:19:18 -0300

    Create a root route with "Hello World" text

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({
+    text: 'Hello World'
+  });
+});
+
 app.listen(4000);
