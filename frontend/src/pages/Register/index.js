de7fd27908efdd257f376727407ec245a6b21817
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-07 23:39:11 -0300

    Fix back link name on Registration page

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -17,7 +17,7 @@ export default function Register() {
 
           <Link to="/" className="back-link">
             <FiArrowLeft size={16} color="#e02041" />
-            Não tenho cadastro
+            Voltar para o logon
           </Link>
         </section>
 
