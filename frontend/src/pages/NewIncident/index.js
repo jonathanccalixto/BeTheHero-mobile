b6de77eb7a09041474ad5c3e70a4172f99461bbf
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-08 09:23:22 -0300

    Defining body at New Incident page

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+
 export default function NewIncident() {
-  return <h1>Novo caso </h1>;
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero"/>
+
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva ocaso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link to="/profile" className="back-link">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Titulo do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
