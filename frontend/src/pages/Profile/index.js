fe32df67d224b2b2ae0b110f9e311d05417c81e8
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-10 01:53:20 -0300

    Get incidents on load Profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
 import './styles.css';
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+  const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    api.get('profiles', {
+      headers: { Authorization: ongId }
+    }).then((response) => {
+      setIncidents(response.data);
+    });
+  }, [ongId]);
 
   return (
     <div className="profile-container">
