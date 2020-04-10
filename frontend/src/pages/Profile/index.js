620e52966ede774874e47683f0d751d89b0bf9eb
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-10 02:27:46 -0300

    Implement logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 import logoImg from '../../assets/logo.svg';
@@ -20,6 +20,8 @@ export default function Profile() {
     });
   }, [ongId]);
 
+  const history = useHistory();
+
   async function handleDelete (id) {
     try {
       const response = await api.delete(`incidents/${id}`, {
@@ -35,6 +37,15 @@ export default function Profile() {
     }
   }
 
+  function handleLogout (event) {
+    event.preventDefault();
+
+    localStorage.removeItem('ongId');
+    localStorage.removeItem('ongName');
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -42,7 +53,7 @@ export default function Profile() {
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
