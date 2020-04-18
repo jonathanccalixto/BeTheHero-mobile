a756473014444a792550a8901bc564160e99c694
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-18 18:39:26 -0300

    Make page structure for Detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Detail() {
+  const item = { id: 1, name: 'APAD', title: 'Cadelinha atropelada', value: 'R$ 120,00' };
   return (
-    <View />
+    <View style={styles.container} >
+      <View style={styles.header} >
+        <Image source={logoImg} />
+        <TouchableOpacity
+          style={styles.detailsButton}
+          onPress={() => {}}
+        >
+          <Feather name="arrow-left" size={28} color="#E02041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={styles.incidentProperty}>ONG:</Text>
+        <Text style={styles.incidentValue}>{item.name}</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>{item.title}</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>{ item.value }</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o héroi deste caso.</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity
+            style={styles.action}
+            onPress={() => {}}
+          >
+            <Text style={styles.actionText}>Whatsapp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.action}
+            onPress={() => {}}
+          >
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
