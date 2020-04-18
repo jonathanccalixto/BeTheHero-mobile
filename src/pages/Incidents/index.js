cb66b5546d3f84851c598352926244049ff79661
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-18 18:00:14 -0300

    Add incidents scroller

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Incidents() {
+  const incidents = [
+    { id: 1, name: 'APAD', title: 'Cadelinha atropelada', value: 'R$ 120,00' },
+    { id: 2, name: 'APAD', title: 'Cadelinha atropelada', value: 'R$ 120,00' },
+    { id: 3, name: 'APAD', title: 'Cadelinha atropelada', value: 'R$ 120,00' },
+  ];
+
   return (
     <View style={styles.container} >
       <View style={styles.header} >
@@ -19,26 +25,32 @@ export default function Incidents() {
       <Text style={styles.title}>Bem-vindo!</Text>
       <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
-      <View style={styles.incidentList}>
-        <View style={styles.incident}>
-          <Text style={styles.incidentProperty}>ONG:</Text>
-          <Text style={styles.incidentValue}>APAD</Text>
+      <FlatList
+        data={incidents}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>{item.name}</Text>
 
-          <Text style={styles.incidentProperty}>CASO:</Text>
-          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>{item.title}</Text>
 
-          <Text style={styles.incidentProperty}>VALOR:</Text>
-          <Text style={styles.incidentValue}>R$ 120,00</Text>
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>{ item.value }</Text>
 
-          <TouchableOpacity
-            style={styles.detailsButton}
-            onPress={() => {}}
-          >
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-            <Feather name="arrow-right" size={16} color="#E02041" />
-          </TouchableOpacity>
-        </View>
-      </View>
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => {}}
+            >
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
