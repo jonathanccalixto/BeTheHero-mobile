e29483a431caac8fd189aeaa252eb95f9e7a4b5a
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-18 18:17:17 -0300

    Add navigation to Detail from Incidents

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
@@ -12,6 +13,12 @@ export default function Incidents() {
     { id: 2, name: 'APAD', title: 'Cadelinha atropelada', value: 'R$ 120,00' },
     { id: 3, name: 'APAD', title: 'Cadelinha atropelada', value: 'R$ 120,00' },
   ];
+
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
 
   return (
     <View style={styles.container} >
@@ -43,7 +50,7 @@ export default function Incidents() {
 
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={() => {}}
+              onPress={navigateToDetail}
             >
               <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
               <Feather name="arrow-right" size={16} color="#E02041" />
