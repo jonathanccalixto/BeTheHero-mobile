1d9ff63fd521064722573d750275bcadb022df25
Jonathan C. Calixto <jonathan@rateiodigital.com.br>
2020-05-01 15:47:50 -0300

    Fix db connection

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const config = (process.env === test ? configuration.test : configuration.development);
+const config = (process.env === 'test' ? configuration.test : configuration.development);
 
 const connection = knex(config);
 
